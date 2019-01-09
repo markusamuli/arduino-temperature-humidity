bfc0f2e1d78483cf1718d5b146b3f4553b9d123a
Markus Ryöti <markus.ryoti@student.tut.fi>
2019-01-09 23:43:40 +0200

    Time tracking is now implemented

--- TemperatureHumidityLogger/TemperatureHumidityLogger.ino
+++ TemperatureHumidityLogger/TemperatureHumidityLogger.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <TimeLib.h>
 #include <dht.h>
 
 
@@ -7,7 +8,16 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 dht DHT;
 
 #define DHT11_PIN 7
+// For creating time request
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message
 
+// State variable will keep track on the display view
+// 0 for "Home View, current humidity and temp"
+// 1 for Historical average
+int viewState = 0;
+// Button press state
+int switchState = 0;
 
 // Measured and calculated values
 struct MeasurementValues
@@ -25,16 +35,10 @@ struct AverageValues
   float humidities[30];
 };
 
+// Initialize all to zero
 MeasurementValues values = {0.0, 0.0, 0.0, 0.0};
 // Values set for debugging
 AverageValues averages = { {21.0, 17.9, 23.2, 19.8, 22.1}, {37.2, 28.4, 19.9, 27.3, 25.0} };
-
-// State variable will keep track on the display view
-// 0 for "Home View, current humidity and temp"
-// 1 for Historical average
-int viewState = 0;
-// Button press state
-int switchState = 0;
 
 
 void updateCurrentValues()
@@ -55,13 +59,16 @@ void calculateAverages()
   int tempValidCount = 0;
   int humValidCount = 0;
   
-  for (int i = 0; i < 30; i++) {
-    if (averages.temperatures[i] != 0) {
+  for (int i = 0; i < 30; i++) 
+  {
+    if (averages.temperatures[i] != 0)
+    {
       tempTotal += averages.temperatures[i];
       tempValidCount++;
     }
     
-    if (averages.humidities[i] != 0) {
+    if (averages.humidities[i] != 0)
+    {
       humTotal += averages.humidities[i];
       humValidCount++;
     }
@@ -74,18 +81,12 @@ void calculateAverages()
 
 void printCurrentValues()
 {
-  // Print also to Serial
-  Serial.print("Temperature: ");
-  Serial.println(values.currentTemp);
-  Serial.print("Humidity: ");
-  Serial.println(values.currentHumidity);
-  
   // Update the LCD values
   lcd.setCursor(0,0); 
   lcd.print("Temp: ");
   lcd.print(values.currentTemp);
   //lcd.print(char(223));
-  lcd.print(" C");
+  lcd.print(" °C");
 
   lcd.setCursor(0,1); 
   lcd.print("Humidity: ");
@@ -111,10 +112,42 @@ void printAverages()
 }
 
 
+// For setting up time
+// Copied directly from library example
+void processSyncMessage()
+{
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if (Serial.find(TIME_HEADER))
+  {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME)
+     { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+}
+
+
+// Request for pc to get the time
+// Copied directly from library example
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
+
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(2, INPUT);
+
+  // For setting up the time
+  setSyncProvider(requestSync);  // set function to call when sync required
+  Serial.println("Waiting for sync message");
+
   lcd.begin(16, 2);
   
   updateCurrentValues();
@@ -129,6 +162,37 @@ void loop()
   static bool validCurrents = false;
   static bool validAverages = false;
   static bool clearedScreen = false;
+  static bool timeSynced = false;
+  static int previousHour = 0;
+  static int prevSecond = 0;
+
+  // Check if the time has been synced by the pc
+  if (!timeSynced)
+  {
+    if (Serial.available())
+    {
+      // Sync needs to happen only once
+      Serial.println("Processing sync message");
+      processSyncMessage();
+      previousHour = hour();
+      timeSynced = true;
+      Serial.println("Time has been synced to the board");
+    }
+  }
+
+  if (prevSecond - second() > 15 || second() - prevSecond > 15)
+  {
+    updateCurrentValues();
+    prevSecond = second();
+  }
+
+  // TODO - get measurements during the day -> add to the array later that day
+  if (previousHour != hour())
+  {
+    // Take measurement
+    // Update to the arrays for the current day
+    previousHour = hour();
+  }
   
   // Check the switch position
   switchState = digitalRead(6);
@@ -138,7 +202,8 @@ void loop()
   // This triggers the change for a differen view in LCD
   if (switchState == HIGH)
   {
-    if (viewState == 0) {
+    if (viewState == 0)
+    {
       viewState = 1;
       lcd.clear();
     } else {
@@ -151,7 +216,8 @@ void loop()
   // Current temperatures will be displayed if state is 0
   if (viewState == 0)
   {
-    if (!validCurrents) {
+    if (!validCurrents)
+    {
       updateCurrentValues();
       validCurrents = true;
     }
@@ -159,12 +225,11 @@ void loop()
     printCurrentValues();
   }
   
-  
-  // TODO - get measurements during the day -> add to the array later that day
   // Average values from the last 30 days will be printed
   if (viewState == 1)
   {
-    if (!validAverages) {
+    if (!validAverages)
+    {
       calculateAverages();
       validAverages = true;
     }
